d775c20f0a4521923f614f8d17c1ddd785b5be8b
MohammadAmin Vahedinia <mohammadamin@beeit.ir>
2017-03-15 17:31:35 +0330

    Error handling in cron view

--- beepaste/beepaste/views/cron/views.py
+++ beepaste/beepaste/views/cron/views.py
@@ -5,7 +5,7 @@ from beepaste.pasteFunctions import pasteExists
 from pyramid.httpexceptions import HTTPNotFound, HTTPFound
 import base64, datetime
 
-@view_config(route_name='schedeuled_remove', renderer='templates/pasteView.jinja2')
+@view_config(route_name='schedeuled_remove')
 def viewPaste(request):
     cronkey = request.matchdict['cronkey']
     if cronkey != request.registry.settings['beepaste.cronkey']:
@@ -15,16 +15,16 @@ def viewPaste(request):
 
         return resp
 
-    #try:
-    cur = datetime.datetime.utcnow()
-    pastes = request.dbsession.query(Pastes).filter(Pastes.expire <= cur).filter(Pastes.toexpire == True).delete()
+    try:
+        cur = datetime.datetime.utcnow()
+        pastes = request.dbsession.query(Pastes).filter(Pastes.expire <= cur).filter(Pastes.toexpire == True).delete()
 
-    resp = Response()
-    resp.status_int = 200
-    resp.text = "done!"
-    #except:
-    #    resp = Response()
-    #    resp.status_int = 409
-    #    resp.text = "some error occured"
+        resp = Response()
+        resp.status_int = 200
+        resp.text = "done!"
+    except:
+        resp = Response()
+        resp.status_int = 409
+        resp.text = "some error occured"
 
     return resp
